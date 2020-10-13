0a93482df147477cd70e1aad08162bfa6c88fd62
pajid9763 <pajid9763@wrdsb.ca>
2020-10-13 12:33:40 +0000

    complete

--- Main.java
+++ Main.java
@@ -3,8 +3,11 @@
  * @author Darian
  */
 public class Main {
+  // method factors
   public static void factors(int num){
+    // this will check every number to see if it is divisible by num
     for (int i = 1; i <= num; i++){
+      // if the number is divisble, print it to screen
       if (num % i == 0){
         System.out.println(i);
       }
@@ -16,6 +19,7 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
+    // call out the method
     factors(30);
     
   }
